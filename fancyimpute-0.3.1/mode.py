44a2eb8aa9d5c06bae033448e61a54242290b0dd
hoa-pham <hpham24@gsu.edu>
2018-04-12 09:41:46 -0400

    almost done

--- fancyimpute-0.3.1/mode.py
+++ fancyimpute-0.3.1/mode.py
@@ -1,11 +1,15 @@
-import sys
-fh = open("data-3.txt", "r+")
-f = open("mode-data-3.txt", "w+")
+f1 = open("data-1.txt", "r+")
+f2 = open("data-2.txt", "r+")
+f3 = open("data-3.txt", "r+")
+
+fh1 = open("mode-data-1.txt", "w+")
+fh2 = open("mode-data-2.txt", "w+")
+fh3 = open("mode-data-3.txt", "w+")
 
 table = []
 
 #appending data to a 2D array table
-for line in fh:
+for line in f1:
     line = line.rstrip('\n').split()
     table.append(line) 
 del table[-1]
@@ -42,6 +46,57 @@ for row in table:
 #writting svm to a file
 for row in table:
     for data in row:
-	    f.write('%s '%(data)),
-    f.writelines('\n')
-f.close()
+	    fh1.write('%s '%(data)),
+    fh1.writelines('\n')
+fh1.close()
+del table[:]
+
+
+#appending data to a 2D array table
+for line in f2:
+    line = line.rstrip('\n').split()
+    table.append(line) 
+del table[-1]
+
+ 
+#filling "NaN" with attribute average value
+for row in table:
+    i = 0
+    for col in row:
+	if col == 'NaN':
+	    mode(table, i)
+	    break
+	i+=1
+	
+#writting svm to a file
+for row in table:
+    for data in row:
+	    fh2.write('%s '%(data)),
+    fh2.writelines('\n')
+fh2.close()
+del table[:]
+
+#appending data to a 2D array table
+for line in f3:
+    line = line.rstrip('\n').split()
+    table.append(line) 
+del table[-1]
+
+ 
+#filling "NaN" with attribute average value
+for row in table:
+    i = 0
+    for col in row:
+	if col == 'NaN':
+	    mode(table, i)
+	    break
+	i+=1
+	
+#writting svm to a file
+for row in table:
+    for data in row:
+	    fh3.write('%s '%(data)),
+    fh3.writelines('\n')
+fh3.close()
+del table[:]
+
--- fancyimpute-0.3.1/svd.py
+++ fancyimpute-0.3.1/svd.py
@@ -1,54 +1,50 @@
-import sys
+from fancyimpute import IterativeSVD
 f1 = open("data-1.txt", "r+")
 f2 = open("data-2.txt", "r+")
 f3 = open("data-3.txt", "r+")
+fh1 = open("svd-data-1.txt", "w+")
+fh2 = open("svd-data-2.txt", "w+")
+fh3 = open("svd-data-3.txt", "w+")
 
-fh1 = open("knn-data-1.txt", "w+")
-fh2 = open("knn-data-2.txt", "w+")
-fh3 = open("knn-data-3.txt", "w+")
-
-from fancyimpute import KNN
 table = []
-
 for line in f1:
     line = line.rstrip('\n').split()
     table.append(line)
 
-filled = KNN(k=3).complete(table)
+filled = IterativeSVD().complete(table)
 
 for row in filled:
     for data in row:
-        fh1.write('%s '%data)
+        fh1.write('%s ' %data)
     fh1.writelines('\n')
 fh1.close()
 del table[:]
 
 table = []
-
 for line in f2:
     line = line.rstrip('\n').split()
     table.append(line)
 
-filled2 = KNN(k=3).complete(table)
+filled2 = IterativeSVD().complete(table)
 
 for row in filled2:
     for data in row:
-        fh2.write('%s '%data)
+        fh2.write('%s ' %data)
     fh2.writelines('\n')
 fh2.close()
 del table[:]
 
 table = []
-
 for line in f3:
     line = line.rstrip('\n').split()
     table.append(line)
 
-filled3 = KNN(k=3).complete(table)
+filled3 = IterativeSVD().complete(table)
 
 for row in filled3:
     for data in row:
-        fh3.write('%s '%data)
+        fh3.write('%s ' %data)
     fh3.writelines('\n')
 fh3.close()
 del table[:]
+
