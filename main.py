de525f2f5d765eb4324084e222159033085059ad
Naman Jain <naman5jain4@gmail.com>
2018-04-12 17:17:00 -0400

    Fixed crash @ Data Set 2
    Now Crashing @ Data Set 4

--- main.py
+++ main.py
@@ -5,22 +5,19 @@ import sklearn
 import sklearn.model_selection
 import sklearn.neighbors
 import sklearn.svm
+import fileinput
 
 
 def main():
     # Training Data
     # Data 1 Learning
-    txt_file21 = r"TrainData1.txt"
-    csv_file21 = r"TrainData1_noNaN.csv"
-    in_txt21 = csv.reader(open(txt_file21, "rt"), delimiter='\t')
-    out_csv21 = csv.writer(open(csv_file21, 'wt'))
-    out_csv21.writerows(in_txt21)
-    with open("TrainData1_noNaN.csv", "rt") as fin:
-        with open("TrainData1.csv", "wt") as fout:
-            for line in fin:
-                fout.write(line.replace('1.00000000000000e+99', 'NaN'))
+    with open('TrainData1.txt', 'r') as file:
+        filedata = file.read()
+    filedata = filedata.replace('1.00000000000000e+99', 'NaN')
+    with open('TrainData1_NaN.txt', 'w') as file:
+        file.write(filedata)
     # Save imputed data to file and keep in dataframe for computation
-    datasetlearn1 = pandas.read_csv('TrainData1.csv', header=None, index_col=None)
+    datasetlearn1 = pandas.read_csv('TrainData1_NaN.txt', header=None, index_col=None, delimiter='\t')
     datasetlearn1.fillna(datasetlearn1.mean(), inplace=True)
     datasetlearn1.to_csv('TrainData1.csv', index=False, header=None)
     datasetlabels1 = pandas.read_csv('TrainLabel1.txt', header=None, index_col=None)
@@ -32,7 +29,6 @@ def main():
     knn1.fit(x_train1, y_train1)
     print("Estimated accuracy for dataset 1 label prediction via KNN, 1:4 sample:")
     print(knn1.score(x_test1, y_test1))
-
     # Use SVM
     svm1 = sklearn.svm.SVC()
     svm1.fit(x_train1, y_train1)
@@ -40,19 +36,16 @@ def main():
     print(svm1.score(x_test1, y_test1))
 
     print("After repeated testing, the chosen algorithm for this data set is KNN. Please see report for more details.")
+    print("")
 
     # Data 2 Learning
-    txt_file22 = r"TrainData2.txt"
-    csv_file22 = r"TrainData2_noNaN.csv"
-    in_txt22 = csv.reader(open(txt_file22, "rt"), delimiter='\t')
-    out_csv22 = csv.writer(open(csv_file22, 'wt'))
-    out_csv22.writerows(in_txt22)
-    with open("TrainData2_noNaN.csv", "rt") as fin:
-        with open("TrainData2.csv", "wt") as fout:
-            for line in fin:
-                fout.write(line.replace('1.00000000000000e+99', 'NaN'))
+    with open('TrainData2.txt', 'r') as file:
+        filedata = file.read()
+    filedata = filedata.replace('1.00000000000000e+99', 'NaN')
+    with open('TrainData2_NaN.txt', 'w') as file:
+        file.write(filedata)
     # Save imputed data to file and keep in dataframe for computation
-    datasetlearn2 = pandas.read_csv('TrainData2.csv', names=None, header=None)
+    datasetlearn2 = pandas.read_csv('TrainData2_NaN.txt', names=None, header=None, delimiter='\t')
     datasetlearn2.fillna(datasetlearn2.mean(), inplace=True)
     datasetlearn2.to_csv('TrainData2.csv', index=False, header=None)
     datasetlabels2 = pandas.read_csv('TrainLabel2.txt', names=None, header=None)
@@ -64,7 +57,6 @@ def main():
     knn2.fit(x_train2, y_train2)
     print("Estimated accuracy for dataset 2 label prediction via KNN, 1:4 sample:")
     print(knn2.score(x_test2, y_test2))
-
     # Use SVM
     svm2 = sklearn.svm.SVC()
     svm2.fit(x_train2, y_train2)
@@ -72,19 +64,16 @@ def main():
     print(svm2.score(x_test2, y_test2))
 
     print("After repeated testing, the chosen algorithm for this data set is ---. Please see report for more details.")
+    print("")
 
     # Data 3 Learning
-    txt_file23 = r"TrainData3.txt"
-    csv_file23 = r"TrainData3_noNaN.csv"
-    in_txt23 = csv.reader(open(txt_file23, "rt"), delimiter='\t')
-    out_csv23 = csv.writer(open(csv_file23, 'wt'))
-    out_csv23.writerows(in_txt23)
-    with open("TrainData3_noNaN.csv", "rt") as fin:
-        with open("TrainData3.csv", "wt") as fout:
-            for line in fin:
-                fout.write(line.replace('1.00000000000000e+99', 'NaN'))
+    with open('TrainData3.txt', 'r') as file:
+        filedata = file.read()
+    filedata = filedata.replace('1.00000000000000e+99', 'NaN')
+    with open('TrainData3_NaN.txt', 'w') as file:
+        file.write(filedata)
     # Save imputed data to file and keep in dataframe for computation
-    datasetlearn3 = pandas.read_csv('TrainData3.csv', names=None, header=None)
+    datasetlearn3 = pandas.read_csv('TrainData3_NaN.txt', names=None, header=None, delimiter='\t')
     datasetlearn3.fillna(datasetlearn3.mean(), inplace=True)
     datasetlearn3.to_csv('TrainData3.csv', index=False, header=None)
     datasetlabels3 = pandas.read_csv('TrainLabel3.txt', names=None, header=None)
@@ -104,19 +93,16 @@ def main():
     print(svm3.score(x_test3, y_test3))
 
     print("After repeated testing, the chosen algorithm for this data set is ---. Please see report for more details.")
+    print("")
 
     # Data 4 Learning
-    txt_file24 = r"TrainData4.txt"
-    csv_file24 = r"TrainData4_noNaN.csv"
-    in_txt24 = csv.reader(open(txt_file24, "rt"), delimiter='\t')
-    out_csv24 = csv.writer(open(csv_file24, 'wt'))
-    out_csv24.writerows(in_txt24)
-    with open("TrainData4_noNaN.csv", "rt") as fin:
-        with open("TrainData4.csv", "wt") as fout:
-            for line in fin:
-                fout.write(line.replace('1.00000000000000e+99', 'NaN'))
+    with open('TrainData4.txt', 'r') as file:
+        filedata = file.read()
+    filedata = filedata.replace('1.00000000000000e+99', 'NaN')
+    with open('TrainData4_NaN.txt', 'w') as file:
+        file.write(filedata)
     # Save imputed data to file and keep in dataframe for computation
-    datasetlearn4 = pandas.read_csv('TrainData4.csv', names=None, header=None)
+    datasetlearn4 = pandas.read_csv('TrainData4_NaN.txt', names=None, header=None, delimiter='\t')
     datasetlearn4.fillna(datasetlearn4.mean(), inplace=True)
     datasetlearn4.to_csv('TrainData4.csv', index=False, header=None)
     datasetlabels4 = pandas.read_csv('TrainLabel4.txt', names=None, header=None)
@@ -128,7 +114,6 @@ def main():
     knn4.fit(x_train4, y_train4)
     print("Estimated accuracy for dataset 4 label prediction via KNN, 1:4 sample:")
     print(knn4.score(x_test4, y_test4))
-
     # Use SVM
     svm4 = sklearn.svm.SVC()
     svm4.fit(x_train4, y_train4)
@@ -136,19 +121,16 @@ def main():
     print(svm4.score(x_test4, y_test4))
 
     print("After repeated testing, the chosen algorithm for this data set is ---. Please see report for more details.")
+    print("")
 
     # Data 5 Learning
-    txt_file25 = r"TrainData5.txt"
-    csv_file25 = r"TrainData5_noNaN.csv"
-    in_txt25 = csv.reader(open(txt_file25, "rt"), delimiter='\t')
-    out_csv25 = csv.writer(open(csv_file25, 'wt'))
-    out_csv25.writerows(in_txt25)
-    with open("TrainData5_noNaN.csv", "rt") as fin:
-        with open("TrainData5.csv", "wt") as fout:
-            for line in fin:
-                fout.write(line.replace('1.00000000000000e+99', 'NaN'))
+    with open('TrainData5.txt', 'r') as file:
+        filedata = file.read()
+    filedata = filedata.replace('1.00000000000000e+99', 'NaN')
+    with open('TrainData5_NaN.txt', 'w') as file:
+        file.write(filedata)
     # Save imputed data to file and keep in dataframe for computation
-    datasetlearn5 = pandas.read_csv('TrainData5.csv', names=None, header=None)
+    datasetlearn5 = pandas.read_csv('TrainData5_NaN.txt', names=None, header=None, delimiter='\t')
     datasetlearn5.fillna(datasetlearn5.mean(), inplace=True)
     datasetlearn5.to_csv('TrainData5.csv', index=False, header=None)
     datasetlabels5 = pandas.read_csv('TrainLabel5.txt', names=None, header=None)
@@ -160,7 +142,6 @@ def main():
     knn5.fit(x_train5, y_train5)
     print("Estimated accuracy for dataset 5 label prediction via KNN, 1:4 sample:")
     print(knn5.score(x_test5, y_test5))
-
     # Use SVM
     svm5 = sklearn.svm.SVC()
     svm5.fit(x_train5, y_train5)
@@ -168,20 +149,17 @@ def main():
     print(svm5.score(x_test5, y_test5))
 
     print("After repeated testing, the chosen algorithm for this data set is ---. Please see report for more details.")
+    print("")
 
     # Testing Data
     # Test set 1
-    txt_file31 = r"TestData1.txt"
-    csv_file31 = r"TestData1_noNaN.csv"
-    in_txt31 = csv.reader(open(txt_file31, "rt"), delimiter='\t')
-    out_csv31 = csv.writer(open(csv_file31, 'wt'))
-    out_csv31.writerows(in_txt31)
-    with open("TestData1_noNaN.csv", "rt") as fin:
-        with open("TestData1.csv", "wt") as fout:
-            for line in fin:
-                fout.write(line.replace('1.00000000000000e+99', 'NaN'))
+    with open('TestData1.txt', 'r') as file:
+        filedata = file.read()
+    filedata = filedata.replace('1.00000000000000e+99', 'NaN')
+    with open('TestData1_NaN.txt', 'w') as file:
+        file.write(filedata)
     # Save imputed data to file and keep in dataframe for computation
-    datasettest1 = pandas.read_csv('TestData1.csv')
+    datasettest1 = pandas.read_csv('TestData1_NaN.txt', names=None, header=None, delimiter='\t')
     datasettest1.fillna(datasettest1.mean(), inplace=True)
     datasettest1.to_csv('TestData1.csv')
     # Re-fit with entire learning data set
@@ -192,17 +170,13 @@ def main():
         results1.write("%s\n" % eaLine)
 
     # Test set 2
-    txt_file32 = r"TestData2.txt"
-    csv_file32 = r"TestData2_noNaN.csv"
-    in_txt32 = csv.reader(open(txt_file32, "rt"), delimiter='\t')
-    out_csv32 = csv.writer(open(csv_file32, 'wt'))
-    out_csv32.writerows(in_txt32)
-    with open("TestData2_noNaN.csv", "rt") as fin:
-        with open("TestData2.csv", "wt") as fout:
-            for line in fin:
-                fout.write(line.replace('1.00000000000000e+99', 'NaN'))
+    with open('TestData2.txt', 'r') as file:
+        filedata = file.read()
+    filedata = filedata.replace('1.00000000000000e+99', 'NaN')
+    with open('TestData2_NaN.txt', 'w') as file:
+        file.write(filedata)
     # Save imputed data to file and keep in dataframe for computation
-    datasettest2 = pandas.read_csv('TestData2.csv', names=None, header=None)
+    datasettest2 = pandas.read_csv('TestData2_NaN.txt', names=None, header=None)
     datasettest2.fillna(datasettest2.mean(), inplace=True)
     datasettest2.to_csv('TestData2.csv', index=None, header=None)
     # Re-fit with entire learning data set
@@ -213,17 +187,13 @@ def main():
         results2.write("%s\n" % eaLine)
 
     # Test set 3
-    txt_file33 = r"TestData3.txt"
-    csv_file33 = r"TestData3_noNaN.csv"
-    in_txt33 = csv.reader(open(txt_file33, "rt"), delimiter='\t')
-    out_csv33 = csv.writer(open(csv_file33, 'wt'))
-    out_csv33.writerows(in_txt33)
-    with open("TestData3_noNaN.csv", "rt") as fin:
-        with open("TestData3.csv", "wt") as fout:
-            for line in fin:
-                fout.write(line.replace('1.00000000000000e+99', 'NaN'))
+    with open('TestData3.txt', 'r') as file:
+        filedata = file.read()
+    filedata = filedata.replace('1.00000000000000e+99', 'NaN')
+    with open('TestData3_NaN.txt', 'w') as file:
+        file.write(filedata)
     # Save imputed data to file and keep in dataframe for computation
-    datasettest3 = pandas.read_csv('TestData3.csv', names=None, header=None)
+    datasettest3 = pandas.read_csv('TestData3_NaN.txt', names=None, header=None)
     datasettest3.fillna(datasettest3.mean(), inplace=True)
     datasettest3.to_csv('TestData3.csv', index=None, header=None)
     # Re-fit with entire learning data set
